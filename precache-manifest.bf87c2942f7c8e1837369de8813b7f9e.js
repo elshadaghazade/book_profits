fdd1346239e534c7ab417377e66243e315e3a2a1
Elshad Agayev <elshadaghazade@gmail.com>
2019-11-18 01:19:30 +0400

    commit

---
+++ precache-manifest.bf87c2942f7c8e1837369de8813b7f9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb4194b81a039c2bec32f148e5a6975",
+    "url": "./index.html"
+  },
+  {
+    "revision": "938344d74b283a0e3eb0",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4105e46ae351b297da32",
+    "url": "./static/css/main.f6b07654.chunk.css"
+  },
+  {
+    "revision": "938344d74b283a0e3eb0",
+    "url": "./static/js/2.106ffcdd.chunk.js"
+  },
+  {
+    "revision": "4105e46ae351b297da32",
+    "url": "./static/js/main.d4ab96b7.chunk.js"
+  },
+  {
+    "revision": "8200b9e50facc5ea8cbc",
+    "url": "./static/js/runtime-main.ef144a65.js"
+  }
+]);
