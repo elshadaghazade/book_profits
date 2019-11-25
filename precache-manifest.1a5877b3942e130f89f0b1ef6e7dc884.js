0e2ffbde55b9e4566e364462f7b79feb7286c8a8
Elshad Agayev <elshadaghazade@gmail.com>
2019-11-25 22:35:55 +0400

    commit

---
+++ precache-manifest.1a5877b3942e130f89f0b1ef6e7dc884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f467fba21386a97a9f4cd3071e909b14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ede77bff3c3fe5b78fc",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d4d4057ad7ae75c82e4f",
+    "url": "./static/css/main.1b55a4fc.chunk.css"
+  },
+  {
+    "revision": "5ede77bff3c3fe5b78fc",
+    "url": "./static/js/2.38b97f4f.chunk.js"
+  },
+  {
+    "revision": "d4d4057ad7ae75c82e4f",
+    "url": "./static/js/main.3e186f6f.chunk.js"
+  },
+  {
+    "revision": "8200b9e50facc5ea8cbc",
+    "url": "./static/js/runtime-main.ef144a65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dd76b161aef609e6a161912b0dd7d3a2.js"
+  "./precache-manifest.1a5877b3942e130f89f0b1ef6e7dc884.js"
 );
 
 self.addEventListener('message', (event) => {
