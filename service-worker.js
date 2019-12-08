e2a25e64fe46f0a17629e39a21ecd3bfb7975ca5
Elshad Agayev <elshadaghazade@gmail.com>
2019-12-08 17:59:43 +0400

    commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.92b61cf95534e1ea04fa2b80d60fe6b7.js"
+  "./precache-manifest.54a9037be2b516b9b2f5eb009196827b.js"
 );
 
 self.addEventListener('message', (event) => {
