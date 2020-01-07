50e9d85c93bbafe636d629bfc58b86b2662976bd
Elshad Agayev <elshadaghazade@gmail.com>
2020-01-08 01:02:26 +0400

    commit

---
+++ precache-manifest.d1b536e619a55c4724dd2fc44105554d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84ad866320a3d831ce5159e1fed52a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "737fc4e5d3019909ad9c",
+    "url": "./static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c0168883f30d190089af",
+    "url": "./static/css/main.ad1d78be.chunk.css"
+  },
+  {
+    "revision": "737fc4e5d3019909ad9c",
+    "url": "./static/js/2.d4286cb4.chunk.js"
+  },
+  {
+    "revision": "c0168883f30d190089af",
+    "url": "./static/js/main.1b3827be.chunk.js"
+  },
+  {
+    "revision": "f57f88a7b422bacece64",
+    "url": "./static/js/runtime-main.40da974b.js"
+  },
+  {
+    "revision": "268bcfb771ed179d2f723ee2a1e394ba",
+    "url": "./static/media/powered-by-logo-blue.268bcfb7.png"
+  }
+]);
